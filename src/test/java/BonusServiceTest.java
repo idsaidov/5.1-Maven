b2af17d675683d34bbba9c00af6cac4c6b3daeff
idsaidov <id.saidov@yandex.ru>
2022-08-07 04:22:17 +0300

    Test+

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -35,5 +35,50 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+    @Test
+    void shouldCalculateForRegisteredAndNullExpected() {
+        BonusService service = new BonusService();
 
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredAndNullAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = true;
+        long expected = 300;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredAnd77_77Amount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 77_77;
+        boolean registered = false;
+        long expected = 300;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    
 }
